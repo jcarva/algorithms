8129e4edd65c6d934f72b7cafc58b48227d7637e
Jaelson <jaelsonjunior99@gmail.com>
2016-08-08 17:29:25 -0300

    Update main file to call InsertionSort
    Add comments

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 import sys
 from random import randint
+from algorithms.insertion import Insertion
 
+"Default input size to the sorting algorithms"
 sample_size = 10
+
+"Initialize vector input"
 sample = []
 
+"Check optional argument to update the input size"
 if len(sys.argv) > 1:
     sample_size = int(sys.argv[1]);
 
+"Set input with random numbers"
 for i in xrange(sample_size):
 	sample.append(randint(0,1000))
 
-for number in sample:
-	print(number)
+"Call Algorithms"
+Insertion(sample)
